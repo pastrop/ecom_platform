095042707725ac68424eb6714545a705e457d2ff
pastrop <pas_trop2003@yahoo.com>
2016-02-12 23:20:36 -0500

    Added facebook auth & stripe payments
    Stripe payments won't work without stripe key

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ var bodyparser = require('body-parser');
 var Category = require('../models/category').Category;
 var Product = require('../models/product').Product;
 var User = require('../models/user').User;
+var stripe = require('stripe');
 //var auth = ('./auth').auth;
 
 var stuff = require('../models/category').stuff;//this is just a test line
@@ -125,4 +126,107 @@ api.get('/starwars', function(request, response) {
     });
 //end of authentication ****************************************************
 
+// User / User-card piece
+
+  api.get('/me', function(req, res) { //User handle
+    if (!req.user) {
+      return res.
+//        status(status.UNAUTHORIZED).
+        json({ error: 'Not logged in' });
+    }
+    console.log('got the logged in User!!!!!!');
+    req.user.populate({ path: 'data.cart.product', model: 'Product' },
+      handleOne.bind(null, 'user', res));
+  });
+
+    api.put('/me/cart', function(req, res) {
+      try {
+        var cart = req.body.data.cart;
+      } catch(e) {
+        return res.
+//          status(status.BAD_REQUEST).
+          json({ error: 'No cart specified!' });
+      }
+
+      req.user.data.cart = cart;
+      req.user.save(function(error, user) {
+        if (error) {
+          return res.
+            status(status.INTERNAL_SERVER_ERROR).
+            json({ error: error.toString() });
+        }
+        return res.json({ user: user });
+      });
+    });
+
+api.post('/checkout', function(req, res) {
+      if (!req.user) {
+        return res.
+          status(status.UNAUTHORIZED).
+          json({ error: 'Not logged in' });
+      }
+
+      // Populate the products in the user's cart
+      req.user.populate({ path: 'data.cart.product', model: 'Product' }, function(error, user) {
+
+        // Sum up the total price in USD
+        var totalCostUSD = 0;
+        _.each(user.data.cart, function(item) {
+          totalCostUSD += item.product.internal.approximatePriceUSD *
+            item.quantity;
+        });
+
+        // And create a charge in Stripe corresponding to the price
+        Stripe.charges.create(
+          {
+            // Stripe wants price in cents, so multiply by 100 and round up
+            amount: Math.ceil(totalCostUSD * 100),
+            currency: 'usd',
+            source: req.body.stripeToken,
+            description: 'Example charge'
+          },
+          function(err, charge) {
+            if (err && err.type === 'StripeCardError') {
+              return res.
+                status(status.BAD_REQUEST).
+                json({ error: err.toString() });
+            }
+            if (err) {
+              console.log(err);
+              return res.
+                status(status.INTERNAL_SERVER_ERROR).
+                json({ error: err.toString() });
+            }
+
+            req.user.data.cart = [];
+            req.user.save(function() {
+              // Ignore any errors - if we failed to empty the user's
+              // cart, that's not necessarily a failure
+
+              // If successful, return the charge id
+              return res.json({ id: charge.id });
+            });
+          });
+      });
+    });//end of checkout
+
+
+
 module.exports = api;
+
+function handleOne(property, res, error, result) {
+  if (error) {
+    return res.
+      status(status.INTERNAL_SERVER_ERROR).
+      json({ error: error.toString() });
+  }
+  if (!result) {
+    return res.
+//      status(status.NOT_FOUND).
+      json({ error: 'Not found - me/cart handle' });
+  }
+
+  var json = {};
+  json[property] = result;
+  res.json(json);
+}
